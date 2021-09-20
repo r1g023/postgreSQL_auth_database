82bd983ea8651d006eb908ea6d14c1b5298da72c
Rigo Limon <limon1285@gmail.com>
2021-09-20 17:01:14 -0500

    ..

--- database/dbConfig.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-const knex = require("knex");
-const config = require("../knexfile");
-
-const enviroment = process.env.NODE_ENV || "development";
-module.exports = knex(config[enviroment]);
